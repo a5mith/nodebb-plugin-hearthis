705c773ec0e63a1c12d4ca1d68046f4788f914b5
a5mith <aaronsmith91@sky.com>
2014-11-23 03:58:43 +0000

    Init

--- library.js
+++ library.js
@@ -2,7 +2,7 @@
     "use strict";
     var HearThis = {},
         EmbedHearThis = '<iframe scrolling="no" id="hearthis_at_track_$1" width="100%" height="400" src="https://hearthis.at/embed/$1/transparent_black/?hcolor=&color=&style=2&block_size=1&block_space=0&background=1&waveform=0&cover=0" frameborder="0" allowtransparency></iframe>';
-    var	HearThisShortLink = /<a href="(?:https?:\/\/)?(?:www\.)?(?:hearthis\.at)\/?([\w\-_]+)">.+<\/a>/g;
+    var	HearThisShortLink = /<a href="(?:https?:\/\/)?(?:www\.)?(?:hearthis\.at)\/?([\w\-_]+)\/?">.+<\/a>/g;
 
 
     HearThis.parse = function(data, callback) {
